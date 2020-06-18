09930de9f1f5f57974bb02e2a0ade725eed1395e
Phen Thach <pshannonthach@gmail.com>
2020-06-17 22:12:32 -0400

    done profile model

---
+++ models/Profile.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const profileSchema = new Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+    },
+    educationLevel: {
+      type: String,
+    },
+    resourceId: {
+      type: [String],
+    },
+    certifications: {
+      type: [String],
+    },
+    city: {
+      type: String,
+    },
+    state: {
+      type: String,
+    },
+    githubUrl: {
+      type: String,
+    },
+    twitterUrl: {
+      type: String,
+    },
+    youtubeUrl: {
+      type: String,
+    },
+    summary: {
+      type: String,
+    },
+  },
+  { timestamps: {} }
+);
