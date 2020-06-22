d6a67c6cfd0ff5aad698f41e8c7678f1609e29c5
Phen Thach <pshannonthach@gmail.com>
2020-06-22 17:00:29 -0400

    profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -20,21 +20,51 @@ router.post(
     check('educationLevel', 'Education level is required').not().isEmpty(),
   ],
   async (req, res) => {
+    console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    try {
+      const {
+        firstName,
+        lastName,
+        name,
+        occupation,
+        educationLevel,
+        resourceId,
+        certifications,
+        location,
+        social,
+      } = req.body;
 
-    const {
-      firstName,
-      lastName,
-      name,
-      occupation,
-      educationLevel,
-      resourceId,
-      certifications,
-      location,
-      social,
-    } = req.body;
+      const userId = req.user.id;
+
+      const profileFields = {
+        firstName,
+        lastName,
+        name,
+      };
+      profileFields.user = userId;
+      if (occupation) profileFields.occupation = occupation;
+      if (educationLevel) profileFields.educationLevel = educationLevel;
+      if (resourceId) profileFields.resourceId = resourceId;
+      if (certifications) profileFields.certifications = certifications;
+      if (location) profileFields.location = location;
+
+      profileFields.social = {};
+      if (githubUrl) profileFields.social.githubUrl = githubUrl;
+      if (twitterUrl) profileFields.social.twitterUrl = twitterUrl;
+      if (youtubeUrl) profileFields.social.youtubeUrl = youtubeUrl;
+
+      profile = new Profile(profileFields);
+      await profile.save();
+      res.json(profile);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
+
+module.exports = router;
