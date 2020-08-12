9f055a022fba2345f7133fdf01c6db951d1de0c2
Phen Thach <pshannonthach@gmail.com>
2020-08-11 20:09:18 -0400

    working on comments route for post

---
+++ routes/api/comments.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const auth = require("../../middleware/auth");
+
+const Post = require('../../models/Post');
+const User = require('../../models/User');
+const Profile = require('../../models/Profile');
+
+// confirm login
+// get the post by post id
+// check if it exists
+// pull the profile from the userid
+// add the comment to the post (text and profileid)
+// confirm the comment was added successfully
+// send the comment array back to the requester
+router.post('/:postID', auth, async (req, res) => {
+try {
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+        return res.status(404).json({msg: "Unable to find post"})
+    }
+    const profile = await Profile.findOne({user: req.user.id});
+    if(!profile) {
+        return res.status(400).json({msg: "Profile required"});
+    }
+    const comment = {text: req.body.text, profile: profile.id};
+    post.comment.push(comment);
+    post.save(post);
+    res.json(post.comments);
+} catch (error) {
+    console.error(error)
+    res.status(500).json(error)
+}
+})
