33401041611f1dc972c08e5efcd216272ad33a4d
Phen Thach <pshannonthach@gmail.com>
2020-08-29 18:52:55 -0400

    store set up

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-router-dom'
+import store from "./store";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode> 
-  <Provider store={}>
+  <Provider store={store}>
     <App />
   </Provider> 
   </React.StrictMode>,
